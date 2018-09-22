185f249a418ceba64430f723ed16ddedda5ce059
lucus <lixin9311@gmail.com>
2018-09-22 10:47:25 +0900

    Allow all origins, ease debug

--- backend/main.go
+++ backend/main.go
@@ -8,9 +8,8 @@ import (
 
 func main() {
 	r := gin.Default()
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{"http://127.0.0.1:4200"}
-	r.Use(cors.New(config))
+	r.Use(cors.Default())
+	r.Static("/login", "./app")
 	r.GET("/challenge", api.ChallengeGet)
 	r.POST("/challenge", api.ChallengePost)
 	r.Run() // listen and serve on 0.0.0.0:8080
